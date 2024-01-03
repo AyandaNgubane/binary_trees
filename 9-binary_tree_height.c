4feeb356ad2fbd6ad8f745f65fd25b8a2042b0c2
Ayanda Ngubane <ayanda4me@gmail.com>
2024-01-03 04:53:26 -0800

    Task 8-9 betty checks

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -7,19 +7,19 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-    if (tree)
-    {
-        size_t left_height = 0, right_height = 0;
+	if (tree)
+	{
+		size_t left_height = 0, right_height = 0;
 
-        if (tree->left)
-            left_height = 1 + binary_tree_height(tree->left);
+		if (tree->left)
+			left_height = 1 + binary_tree_height(tree->left);
 
-        if (tree->right)
-            right_height = 1 + binary_tree_height(tree->right);
+		if (tree->right)
+			right_height = 1 + binary_tree_height(tree->right);
 
-        return (left_height > right_height) ? left_height : right_height;
-    }
+		return ((left_height > right_height) ? left_height : right_height);
+	}
 
-    return 0;
+	return (0);
 }
 
